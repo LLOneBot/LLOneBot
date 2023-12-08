6924c0f9431e35c7971dc40209a9e0caf2208340
husky <3088506834@qq.com>
2023-12-08 23:24:22 +0800

    修改发送消息返回数据

--- src/main/utils.ts
+++ src/main/utils.ts
@@ -1,3 +1,5 @@
+import { text } from "express";
+
 const fs = require('fs');
 
 export function log(msg: any) {
@@ -6,3 +8,38 @@ export function log(msg: any) {
 
     })
 }
+
+// @SiberianHusky 2021-08-15
+export default function judgeMessage(msg) {
+        if(msg[0]!=null&&msg[0]["type"]!=null&&msg[0]["data"]!=null){
+            let type = msg[0]["type"];
+            let data = msg[0]["data"];
+            if(type == "text"){
+                if (data["text"]!=""&&data["text"]!=null){
+                    return 200;
+                }
+                else if(type==="image"){
+                    if(data["file"]!=""&&data["file"]!=null){
+                        return 200;
+                    }
+                }
+                else if(type==="voice"){
+                    if(data["file"]!=""&&data["file"]!=null){
+                        return 200;
+                    }
+                }
+                else if(type==="at"){
+                    if(data["qq"]!=""&&data["qq"]!=null){
+                        return 200;
+                    }
+                }
+                else if(type==="reply"){
+                    if(data["id"]!=""&&data["id"]!=null){
+                        return 200;
+                    }
+                }
+        }
+        return 400;
+    }
+}
+// ==end==
