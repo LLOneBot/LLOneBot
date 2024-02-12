050719fb7526d6b380957ec2f5cfe22e762fb69a
linyuchen <lin.yu.chen@hotmail.com>
2024-02-12 22:12:25 +0800

    feat: delete msg

--- src/onebot11/actions/index.ts
+++ src/onebot11/actions/index.ts
@@ -8,11 +8,13 @@ import GetGroupMemberInfo from './GetGroupMemberInfo'
 import SendGroupMsg from './SendGroupMsg'
 import SendPrivateMsg from './SendPrivateMsg'
 import SendMsg from './SendMsg'
+import DeleteMsg from "./DeleteMsg";
 
 export const actionHandlers = [
     new GetMsg(),
     new GetLoginInfo(),
     new GetFriendList(),
     new GetGroupList(), new GetGroupInfo(), new GetGroupMemberList(), new GetGroupMemberInfo(),
-    new SendGroupMsg(), new SendPrivateMsg(), new SendMsg()
+    new SendGroupMsg(), new SendPrivateMsg(), new SendMsg(),
+    new DeleteMsg()
 ]
