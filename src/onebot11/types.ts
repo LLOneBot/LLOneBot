1266a22ff1230b21df561bbc2ed6fbd763965dd7
Misa Liu <misaliu@misaliu.top>
2024-02-23 11:35:17 +0800

    fix: Fix type of `message.real_id`

--- src/onebot11/types.ts
+++ src/onebot11/types.ts
@@ -59,7 +59,7 @@ export interface OB11Message {
     self_id?: number,
     time: number,
     message_id: number,
-    real_id: string,
+    real_id: string | number,
     user_id: number,
     group_id?: number,
     message_type: "private" | "group",
