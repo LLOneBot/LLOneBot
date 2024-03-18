7fbf39ac9472df85c6d1aa61fbaf22ae737e90ad
linyuchen <lin.yu.chen@foxmail.com>
2024-03-19 00:45:59 +0800

    feat: stranger info add sex & qq_level

--- src/onebot11/action/go-cqhttp/GetStrangerInfo.ts
+++ src/onebot11/action/go-cqhttp/GetStrangerInfo.ts
@@ -5,24 +5,29 @@ import {OB11Constructor} from "../../constructor";
 import {ActionName} from "../types";
 import {isNull, log} from "../../../common/utils";
 import {NTQQUserApi} from "../../../ntqqapi/api/user";
+import {Friend, GroupMember} from "../../../ntqqapi/types";
 
 
 export default class GoCQHTTPGetStrangerInfo extends BaseAction<{ user_id: number }, OB11User> {
     actionName = ActionName.GoCQHTTP_GetStrangerInfo
 
+    private async refreshInfo(user: Friend | GroupMember){
+        if (isNull(user.sex)){
+            let info = (await NTQQUserApi.getUserDetailInfo(user.uid))
+            Object.assign(user, info);
+        }
+    }
     protected async _handle(payload: { user_id: number }): Promise<OB11User> {
         const user_id = payload.user_id.toString()
         const friend = await getFriend(user_id)
         if (friend) {
+            await this.refreshInfo(friend);
             return OB11Constructor.friend(friend);
         }
         for (const group of groups) {
             const member = await getGroupMember(group.groupCode, user_id)
             if (member) {
-                if (isNull(member.sex)){
-                    let info = (await NTQQUserApi.getUserDetailInfo(member.uid))
-                    Object.assign(member, info);
-                }
+                await this.refreshInfo(member);
                 return OB11Constructor.groupMember(group.groupCode, member) as OB11User
             }
         }
