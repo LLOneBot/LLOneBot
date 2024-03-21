ebca6a07c5b4ab000172cd558288be35a92a6c88
linyuchen <lin.yu.chen@foxmail.com>
2024-03-21 13:02:15 +0800

    fix: auto create temp dir

--- src/common/utils/index.ts
+++ src/common/utils/index.ts
@@ -11,7 +11,7 @@ export const DATA_DIR = global.LiteLoader.plugins["LLOneBot"].path.data;
 export const TEMP_DIR = path.join(DATA_DIR, "temp");
 export const PLUGIN_DIR = global.LiteLoader.plugins["LLOneBot"].path.plugin;
 if (!fs.existsSync(TEMP_DIR)) {
-    fs.mkdirSync(TEMP_DIR);
+    fs.mkdirSync(TEMP_DIR, {recursive: true});
 }
 export {getVideoInfo} from "./video";
 export {checkFfmpeg} from "./video";
