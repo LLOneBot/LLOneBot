de4291a77c04b24317da185c8f54474d9c169563
idranme <96647698+idranme@users.noreply.github.com>
2024-05-04 23:25:01 +0800

    Update silk_worker.ts

--- src/common/utils/silk_worker.ts
+++ src/common/utils/silk_worker.ts
@@ -1,45 +1,45 @@
 import { encode, decode, encodeResult, decodeResult } from 'silk-wasm'
-import { isMainThread, parentPort, Worker, MessageChannel } from 'node:worker_threads'
+import { Worker, MessageChannel } from 'node:worker_threads'
 
 interface Data {
     type: string,
     params: [input: ArrayBufferView | ArrayBuffer, sampleRate: number]
 }
 
-if (!isMainThread && parentPort) {
-    parentPort.addListener('message', (e) => {
-        const data: Data = e.data
-        const port: MessagePort = e.port
-        switch (data.type) {
-            case "encode":
-                encode(...data.params)
-                    .then(ret => {
-                        port.postMessage(ret)
-                    }).catch(err => {
-                        port.postMessage(err)
-                    }).finally(() => {
-                        port.close()
-                    })
-                break
-            case "decode":
-                decode(...data.params).then(ret => {
-                    port.postMessage(ret)
-                }).catch(err => {
-                    port.postMessage(err)
-                }).finally(() => {
-                    port.close()
-                })
-                break
-            default:
-                port.postMessage(undefined)
-                port.close()
-        }
-    })
-}
-
 function postMessage(data: Data): Promise<any> {
     return new Promise((resolve, reject) => {
-        const worker = new Worker(__filename)
+        const script = `
+            const { encode, decode } = require('silk-wasm')
+            const { parentPort } = require('node:worker_threads')
+            parentPort.addListener('message', (e) => {
+                const data = e.data
+                const port = e.port
+                switch (data.type) {
+                    case 'encode':
+                        encode(...data.params)
+                            .then(ret => {
+                                port.postMessage(ret)
+                            }).catch(err => {
+                                port.postMessage(err)
+                            }).finally(() => {
+                                port.close()
+                            })
+                        break
+                    case 'decode':
+                        decode(...data.params).then(ret => {
+                            port.postMessage(ret)
+                        }).catch(err => {
+                            port.postMessage(err)
+                        }).finally(() => {
+                            port.close()
+                        })
+                        break
+                    default:
+                        port.postMessage(undefined)
+                        port.close()
+                }
+            })`
+        const worker = new Worker(script, { eval: true })
         const { port1, port2 } = new MessageChannel()
         port2.once('message', async (ret) => {
             port2.close()
