c2d1966979b4cf317f54f00610f40d2c87744315
手瓜一十雪 <nanaeonn@outlook.com>
2024-05-28 17:14:24 +0800

    feat: support qzone cookies

--- src/onebot11/action/user/GetCookie.ts
+++ src/onebot11/action/user/GetCookie.ts
@@ -1,6 +1,6 @@
 import BaseAction from '../BaseAction'
-import { NTQQUserApi } from '../../../ntqqapi/api'
-import { groups } from '../../../common/data'
+import { NTQQUserApi, WebApi } from '../../../ntqqapi/api'
+import { groups, selfInfo } from '../../../common/data'
 import { ActionName } from '../types'
 
 interface Payload {
@@ -12,6 +12,14 @@ export class GetCookies extends BaseAction<Payload, { cookies: string; bkn: stri
 
   protected async _handle(payload: Payload) {
     const domain = payload.domain || 'qun.qq.com'
+    if (domain.endsWith("qzone.qq.com")) {
+      const _Skey = await NTQQUserApi.getSkey() as string;
+      // 兼容整个 *.qzone.qq.com
+      let data = (await NTQQUserApi.getQzoneCookies());
+      const Bkn = WebApi.genBkn(data.p_skey);
+      const CookieValue = 'p_skey=' + data.p_skey + '; skey=' + data.skey + '; p_uin=o' + selfInfo.uin + '; uin=o' + selfInfo.uin;
+      return { bkn: WebApi.genBkn(data.p_skey), cookies: CookieValue };
+    }
     return NTQQUserApi.getCookies(domain);
   }
 }
