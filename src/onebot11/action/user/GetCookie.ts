f78333acb72a92e239d8d237af45c29387e0f2c2
手瓜一十雪 <nanaeonn@outlook.com>
2024-05-29 12:03:35 +0800

    refactor: get cookies

--- src/onebot11/action/user/GetCookie.ts
+++ src/onebot11/action/user/GetCookie.ts
@@ -12,14 +12,6 @@ export class GetCookies extends BaseAction<Payload, { cookies: string; bkn: stri
 
   protected async _handle(payload: Payload) {
     const domain = payload.domain || 'qun.qq.com'
-    if (domain.endsWith("qzone.qq.com")) {
-      const _Skey = await NTQQUserApi.getSkey() as string;
-      // 兼容整个 *.qzone.qq.com
-      let data = (await NTQQUserApi.getQzoneCookies());
-      const Bkn = WebApi.genBkn(data.p_skey);
-      const CookieValue = 'p_skey=' + data.p_skey + '; skey=' + data.skey + '; p_uin=o' + selfInfo.uin + '; uin=o' + selfInfo.uin;
-      return { bkn: WebApi.genBkn(data.p_skey), cookies: CookieValue };
-    }
     return NTQQUserApi.getCookies(domain);
   }
 }
