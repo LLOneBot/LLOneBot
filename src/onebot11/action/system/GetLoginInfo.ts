234167f305107a16f61cff2b58e64fcf5afcbfc4
idranme <idranme@outlook.com>
2024-08-28 16:06:40 +0800

    fix

--- src/onebot11/action/system/GetLoginInfo.ts
+++ src/onebot11/action/system/GetLoginInfo.ts
@@ -1,5 +1,5 @@
-import { OB11User } from '../../types'
 import BaseAction from '../BaseAction'
+import { OB11User } from '../../types'
 import { ActionName } from '../types'
 import { selfInfo } from '@/common/globalVars'
 
@@ -7,9 +7,13 @@ class GetLoginInfo extends BaseAction<null, OB11User> {
   actionName = ActionName.GetLoginInfo
 
   protected async _handle(payload: null) {
+    let nickname = selfInfo.nick
+    try {
+      nickname = await this.ctx.ntUserApi.getSelfNick(true)
+    } catch { }
     return {
       user_id: parseInt(selfInfo.uin),
-      nickname: await this.ctx.ntUserApi.getSelfNick(true)
+      nickname
     }
   }
 }
