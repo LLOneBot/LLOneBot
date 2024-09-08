65508fc06b1649fcaf5aea42dfc4a3ad788cc4c9
idranme <idranme@outlook.com>
2024-09-08 20:50:22 +0800

    feat

--- src/onebot11/action/go-cqhttp/UploadFile.ts
+++ src/onebot11/action/go-cqhttp/UploadFile.ts
@@ -1,8 +1,6 @@
-import fs from 'node:fs'
 import BaseAction from '../BaseAction'
 import { ActionName } from '../types'
 import { SendElementEntities } from '@/ntqqapi/entities'
-import { SendFileElement } from '@/ntqqapi/types'
 import { uri2local } from '@/common/utils'
 import { sendMsg, createPeer, CreatePeerMode } from '../../helper/createMessage'
 
@@ -18,15 +16,11 @@ export class UploadGroupFile extends BaseAction<UploadGroupFilePayload, null> {
   actionName = ActionName.GoCQHTTP_UploadGroupFile
 
   protected async _handle(payload: UploadGroupFilePayload): Promise<null> {
-    let file = payload.file
-    if (fs.existsSync(file)) {
-      file = `file://${file}`
+    const { success, errMsg, path } = await uri2local(payload.file)
+    if (!success) {
+      throw new Error(errMsg)
     }
-    const downloadResult = await uri2local(file)
-    if (!downloadResult.success) {
-      throw new Error(downloadResult.errMsg)
-    }
-    const sendFileEle = await SendElementEntities.file(this.ctx, downloadResult.path, payload.name, payload.folder_id)
+    const sendFileEle = await SendElementEntities.file(this.ctx, path, payload.name, payload.folder_id)
     const peer = await createPeer(this.ctx, payload, CreatePeerMode.Group)
     await sendMsg(this.ctx, peer, [sendFileEle], [])
     return null
@@ -43,16 +37,12 @@ export class UploadPrivateFile extends BaseAction<UploadPrivateFilePayload, null
   actionName = ActionName.GoCQHTTP_UploadPrivateFile
 
   protected async _handle(payload: UploadPrivateFilePayload): Promise<null> {
+    const { success, errMsg, path } = await uri2local(payload.file)
+    if (!success) {
+      throw new Error(errMsg)
+    }
+    const sendFileEle = await SendElementEntities.file(this.ctx, path, payload.name)
     const peer = await createPeer(this.ctx, payload, CreatePeerMode.Private)
-    let file = payload.file
-    if (fs.existsSync(file)) {
-      file = `file://${file}`
-    }
-    const downloadResult = await uri2local(file)
-    if (!downloadResult.success) {
-      throw new Error(downloadResult.errMsg)
-    }
-    const sendFileEle: SendFileElement = await SendElementEntities.file(this.ctx, downloadResult.path, payload.name)
     await sendMsg(this.ctx, peer, [sendFileEle], [])
     return null
   }
