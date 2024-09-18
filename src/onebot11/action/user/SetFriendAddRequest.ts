b368ab4ad5885f8e8620caa4cd7c2e47d5481bd2
idranme <idranme@outlook.com>
2024-09-18 20:47:45 +0800

    feat: support setting remark when agreeing to a friend request

--- src/onebot11/action/user/SetFriendAddRequest.ts
+++ src/onebot11/action/user/SetFriendAddRequest.ts
@@ -12,7 +12,16 @@ export default class SetFriendAddRequest extends BaseAction<Payload, null> {
 
   protected async _handle(payload: Payload): Promise<null> {
     const approve = payload.approve?.toString() !== 'false'
-    await this.ctx.ntFriendApi.handleFriendRequest(payload.flag, approve)
+    const data = payload.flag.split('|')
+    if (data.length < 2) {
+      throw new Error('无效的flag')
+    }
+    const uid = data[0]
+    const reqTime = data[1]
+    await this.ctx.ntFriendApi.handleFriendRequest(uid, reqTime, approve)
+    if (payload.remark) {
+      await this.ctx.ntFriendApi.setBuddyRemark(uid, payload.remark)
+    }
     return null
   }
 }
