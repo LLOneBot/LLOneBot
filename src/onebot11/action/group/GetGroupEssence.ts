558c927c3e1426156f7173e323906598d578377f
idranme <idranme@outlook.com>
2024-09-20 22:55:29 +0800

    feat: `get_essence_msg_list` API

--- src/onebot11/action/group/GetGroupEssence.ts
+++ src/onebot11/action/group/GetGroupEssence.ts
@@ -1,16 +1,50 @@
-import { GroupEssenceMsgRet } from '@/ntqqapi/api'
-import { BaseAction } from '../BaseAction'
+import { BaseAction, Schema } from '../BaseAction'
 import { ActionName } from '../types'
+import { ChatType } from '@/ntqqapi/types'
 
-interface PayloadType {
-  group_id: number
-  pages?: number
+interface Payload {
+  group_id: number | string
 }
 
-export class GetGroupEssence extends BaseAction<PayloadType, GroupEssenceMsgRet | void> {
-  actionName = ActionName.GoCQHTTP_GetEssenceMsg
+interface EssenceMsg {
+  sender_id: number
+  sender_nick: string
+  sender_time: number
+  operator_id: number
+  operator_nick: string
+  operator_time: number
+  message_id: number
+}
 
-  protected async _handle() {
-    throw '此 api 暂不支持'
+export class GetGroupEssence extends BaseAction<Payload, EssenceMsg[]> {
+  actionName = ActionName.GoCQHTTP_GetEssenceMsgList
+  payloadSchema = Schema.object({
+    group_id: Schema.union([Number, String]).required()
+  })
+
+  protected async _handle(payload: Payload) {
+    const groupCode = payload.group_id.toString()
+    const peer = {
+      guildId: '',
+      chatType: ChatType.group,
+      peerUid: groupCode
+    }
+    const essence = await this.ctx.ntGroupApi.queryCachedEssenceMsg(groupCode)
+    const data: EssenceMsg[] = []
+    for (const item of essence.items) {
+      const { msgList } = await this.ctx.ntMsgApi.queryMsgsWithFilterExBySeq(peer, String(item.msgSeq), '0')
+      const sourceMsg = msgList.find(e => e.msgRandom === String(item.msgRandom))
+      if (!sourceMsg) continue
+      data.push({
+        sender_id: +item.msgSenderUin,
+        sender_nick: item.msgSenderNick,
+        sender_time: +sourceMsg.msgTime,
+        operator_id: +item.opUin,
+        operator_nick: item.opNick,
+        operator_time: item.opTime,
+        message_id: this.ctx.store.createMsgShortId(peer, sourceMsg.msgId)
+      })
+    }
+    return data
   }
 }
