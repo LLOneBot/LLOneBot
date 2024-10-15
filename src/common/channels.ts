39fda247996f9171b3bfe94edaf711f9c7bf3516
idranme <96647698+idranme@users.noreply.github.com>
2024-10-15 11:51:00 +0800

    fix: config hot update

--- src/common/channels.ts
+++ src/common/channels.ts
@@ -1,6 +1,5 @@
 export const CHANNEL_GET_CONFIG = 'llonebot_get_config'
 export const CHANNEL_SET_CONFIG = 'llonebot_set_config'
-export const CHANNEL_SET_CONFIG_CONFIRMED = 'llonebot_set_config_confirmed'
 export const CHANNEL_LOG = 'llonebot_log'
 export const CHANNEL_ERROR = 'llonebot_error'
 export const CHANNEL_UPDATE = 'llonebot_update'
