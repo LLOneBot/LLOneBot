8565dcdc48a248b718762676f4f05ef81c6ede6a
idranme <96647698+idranme@users.noreply.github.com>
2024-10-19 18:09:23 +0800

    refactor

--- src/onebot11/event/notice/OB11GroupDecreaseEvent.ts
+++ src/onebot11/event/notice/OB11GroupDecreaseEvent.ts
@@ -12,7 +12,7 @@ export class OB11GroupDecreaseEvent extends OB11GroupNoticeEvent {
   constructor(groupId: number, userId: number, operatorId: number, subType: GroupDecreaseSubType = 'leave') {
     super()
     this.group_id = groupId
-    this.operator_id = operatorId // 实际上不应该这么实现，但是现在还没有办法识别用户是被踢出的，还是自己主动退出的
+    this.operator_id = operatorId
     this.user_id = userId
     this.sub_type = subType
   }
