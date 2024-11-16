c64e2442ab0b440e91d6379ccc5c155af7ccd3ff
idranme <96647698+idranme@users.noreply.github.com>
2024-11-16 19:39:14 +0800

    refactor

--- src/onebot11/action/llonebot/SetFriendRemark.ts
+++ src/onebot11/action/llonebot/SetFriendRemark.ts
@@ -1,15 +1,19 @@
-import { BaseAction } from '../BaseAction'
+import { BaseAction, Schema } from '../BaseAction'
 import { ActionName } from '../types'
 
 interface Payload {
-  user_id: string,
+  user_id: number | string
   remark?: string
 }
 
-export class SetFriendRemark extends BaseAction<Payload, null> {
+export class SetFriendRemark extends BaseAction<Payload, unknown> {
   actionName = ActionName.SetFriendRemark
+  payloadSchema = Schema.object({
+    user_id: Schema.union([Number, String]).required(),
+    remark: Schema.string()
+  })
 
-  protected async _handle(payload: Payload): Promise<null> {
+  protected async _handle(payload: Payload) {
     const uid = await this.ctx.ntUserApi.getUidByUin(payload.user_id.toString())
     if (!uid) throw new Error('无法获取好友信息')
     return this.ctx.ntFriendApi.setBuddyRemark(uid, payload.remark || '')
