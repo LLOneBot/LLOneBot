46d9b585a0f1ac1368ac74e92febe575fea9b301
linyuchen <lin.yu.chen@hotmail.com>
2024-12-03 18:07:07 +0800

    bump version

--- src/onebot11/action/llonebot/FriendPoke.ts
+++ src/onebot11/action/llonebot/FriendPoke.ts
@@ -15,7 +15,7 @@ export class FriendPoke extends BaseAction<Payload, null> {
   async _handle(payload: Payload) {
     if (!this.ctx.app.native.checkPlatform() || !this.ctx.app.native.checkVersion()) {
       // await this.ctx.app.packet.sendPokePacket(+payload.user_id)
-      throw new Error('戳一戳暂时只支持Windows QQ 27333 ~ 275970版本')
+      throw new Error('戳一戳暂时只支持Windows QQ 27333 ~ 27597版本')
     }
     else{
       await this.ctx.app.native.sendFriendPoke(+payload.user_id)
