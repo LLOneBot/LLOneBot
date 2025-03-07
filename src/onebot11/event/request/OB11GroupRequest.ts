41d39edf0a889e1adb91ecc03c3908e43e98d44f
linyuchen <lin.yu.chen@hotmail.com>
2025-03-07 14:18:27 +0800

    fix: invitor_id

--- src/onebot11/event/request/OB11GroupRequest.ts
+++ src/onebot11/event/request/OB11GroupRequest.ts
@@ -4,18 +4,20 @@ import { EventType } from '../OB11BaseEvent'
 export class OB11GroupRequestEvent extends OB11BaseNoticeEvent {
   post_type = EventType.REQUEST
   request_type = 'group'
-  sub_type: 'add' | 'invite'
+  sub_type: 'add' | 'invite'  // invite 为邀请 bot 进群
   comment: string
   flag: string
   group_id: number
-  user_id: number
+  user_id: number  // 当 sub_type 为 invite 的时候， user_id 为邀请人的 QQ 号
+  invitor_id: number // https://github.com/Mrs4s/go-cqhttp/blob/master/coolq/event.go#L566
 
-  constructor(groupId: number, userId: number, flag: string, comment: string, subType: 'add' | 'invite') {
+  constructor(groupId: number, userId: number, flag: string, comment: string, subType: 'add' | 'invite', invitorId: number = 0) {
     super()
     this.group_id = groupId
     this.user_id = userId
     this.comment = comment
     this.flag = flag
     this.sub_type = subType
+    this.invitor_id = invitorId
   }
 }
