6d9aba4118c7abf50607d84dff37a10208edb6b2
linyuchen <lin.yu.chen@hotmail.com>
2025-06-11 03:41:07 +0800

    chore: enhance vite config to dynamically retrieve module dependencies and streamline external module handling

--- vite.config.ts
+++ vite.config.ts
@@ -2,24 +2,43 @@ import { defineConfig } from 'vite'
 import { builtinModules } from 'module'
 import cp from 'vite-plugin-cp'
 import { writeVersion } from './src/version'
+import path from 'node:path'
+import fs from 'node:fs'
 
 writeVersion()
+
+function getModuleDependencies(moduleName: string, basePath = path.join(__dirname, 'node_modules'), seen = new Set<string>()) {
+  if (seen.has(moduleName)) {
+    return [];
+  }
+  seen.add(moduleName);
+
+  const pkgPath = path.join(basePath, moduleName, 'package.json');
+  let pkg;
+  try {
+    const content = fs.readFileSync(pkgPath, 'utf-8');
+    pkg = JSON.parse(content);
+  } catch (err) {
+    // 找不到 package.json 或 JSON 解析失败时，跳过
+    return [];
+  }
+
+  const deps = Object.keys(pkg.dependencies || {});
+  for (const dep of deps) {
+    getModuleDependencies(dep, basePath, seen);
+  }
+
+  const result = Array.from(seen);
+  return result
+}
 
 const external = [
   'ws',
   'silk-wasm',
   '@minatojs/sql.js',
-  'file-type',
-  'uint8array-extras',
-  'strtok3',
-  'token-types',
-  '@tokenizer/inflate',
-  'peek-readable',
-  'ieee754',
-  'fflate',
-  'debug',
-  'ms',
+  ...getModuleDependencies('file-type'),
 ]
+// console.log(external)
 
 function genCpModule(module: string | RegExp) {
   return { src: `./node_modules/${module}`, dest: `dist/node_modules/${module}`, flatten: false }
