74592799c515aa94951c2aa3b77a55fd592a4770
idranme <96647698+idranme@users.noreply.github.com>
2025-09-27 15:00:51 +0800

    chore: bump version

--- src/version.ts
+++ src/version.ts
@@ -1,7 +1,7 @@
 import fs from 'fs'
 import packageJson from '../package-dist.json'
 
-export const version = '6.1.1'
+export const version = '6.2.0'
 
 export const writeVersion = ()=>{
   const pkgJsonPath = './package-dist.json'
