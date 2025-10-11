5cd90c799e52ce4f61bdb02b3598f746b4c60713
idranme <96647698+idranme@users.noreply.github.com>
2025-10-12 00:14:05 +0800

    enhance

--- src/ntqqapi/entities.ts
+++ src/ntqqapi/entities.ts
@@ -95,7 +95,7 @@ export namespace SendElement {
     }
   }
 
-  export async function file(ctx: Context, filePath: string, fileName: string, folderId = ''): Promise<SendFileElement> {
+  export async function file(ctx: Context, filePath: string, fileName: string, folderId?: string): Promise<SendFileElement> {
     const fileSize = (await stat(filePath)).size.toString()
     if (fileSize === '0') {
       ctx.logger.warn(`文件${fileName}异常，大小为 0`)
