415cb37b1a2417087f3b559a7ac65e3cd385f19a
idranme <96647698+idranme@users.noreply.github.com>
2025-10-26 00:56:50 +0800

    refactor

--- src/onebot11/action/group/GetGroupMemberList.ts
+++ src/onebot11/action/group/GetGroupMemberList.ts
@@ -2,44 +2,23 @@ import { OB11GroupMember } from '../../types'
 import { OB11Entities } from '../../entities'
 import { BaseAction, Schema } from '../BaseAction'
 import { ActionName } from '../types'
-import { parseBool } from '@/common/utils/misc'
 
 interface Payload {
   group_id: number | string
-  no_cache: boolean
 }
 
 class GetGroupMemberList extends BaseAction<Payload, OB11GroupMember[]> {
   actionName = ActionName.GetGroupMemberList
   payloadSchema = Schema.object({
-    group_id: Schema.union([Number, String]).required(),
-    no_cache: Schema.union([Boolean, Schema.transform(String, parseBool)]).default(false)
+    group_id: Schema.union([Number, String]).required()
   })
 
   protected async _handle(payload: Payload) {
     const groupCode = payload.group_id.toString()
     // 使用缓存可能导致群成员列表不完整
-    let groupMembers = await this.ctx.ntGroupApi.getGroupMembers(groupCode)
-    for (let i = 0; i < 5; i++) {
-      if (groupMembers.size > 0) {
-        break
-      }
-      await this.ctx.sleep(60)
-      groupMembers = await this.ctx.ntGroupApi.getGroupMembers(groupCode, payload.no_cache)
-    }
-
-    const date = Math.trunc(Date.now() / 1000)
+    const groupMembers = await this.ctx.ntGroupApi.getGroupMembers(groupCode)
     const groupId = Number(payload.group_id)
-    const ret: OB11GroupMember[] = []
-
-    for (const item of groupMembers.values()) {
-      const member = OB11Entities.groupMember(groupId, item)
-      member.join_time ??= date
-      member.last_sent_time ??= date
-      ret.push(member)
-    }
-
-    return ret
+    return groupMembers.values().map(e => OB11Entities.groupMember(groupId, e)).toArray()
   }
 }
 
