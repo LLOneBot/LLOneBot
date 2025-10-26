be8f75826be500d7585f0af100ba314fab4766dd
idranme <96647698+idranme@users.noreply.github.com>
2025-10-26 16:45:56 +0800

    refactor

--- src/onebot11/action/group/GetGroupMemberInfo.ts
+++ src/onebot11/action/group/GetGroupMemberInfo.ts
@@ -24,31 +24,25 @@ class GetGroupMemberInfo extends BaseAction<Payload, OB11GroupMember> {
     const uid = await this.ctx.ntUserApi.getUidByUin(payload.user_id.toString(), groupCode)
     if (!uid) throw new Error('无法获取用户信息')
     const member = await this.ctx.ntGroupApi.getGroupMember(groupCode, uid, payload.no_cache)
-    if (member) {
-      const ret = OB11Entities.groupMember(+groupCode, member)
-      const date = Math.trunc(Date.now() / 1000)
-      ret.last_sent_time ??= date
-      ret.join_time ??= date
-      let info: UserDetailInfo | null = null
+    const ret = OB11Entities.groupMember(+groupCode, member)
+    let info: UserDetailInfo | undefined
+    try {
+      info = await this.ctx.ntUserApi.getUserDetailInfoWithBizInfo(member.uid)
+    } catch (e) {
       try {
-        info = await this.ctx.ntUserApi.getUserDetailInfoWithBizInfo(member.uid)
-      } catch (e) {
-        try {
-          const fetchInfo = await this.ctx.ntUserApi.fetchUserDetailInfo(member.uid)
-          if (fetchInfo) {
-            info = fetchInfo
-          }
-        } catch (e) {
+        const fetchInfo = await this.ctx.ntUserApi.fetchUserDetailInfo(member.uid)
+        if (fetchInfo) {
+          info = fetchInfo
         }
+      } catch (e) {
       }
-      if (info) {
-        ret.sex = OB11Entities.sex(info.simpleInfo.baseInfo.sex)
-        ret.qq_level = info.commonExt?.qqLevel && calcQQLevel(info.commonExt.qqLevel) || 0
-        ret.age = info.simpleInfo.baseInfo.age ?? 0
-      }
-      return ret
     }
-    throw new Error(`群成员${payload.user_id}不存在`)
+    if (info) {
+      ret.sex = OB11Entities.sex(info.simpleInfo.baseInfo.sex)
+      ret.qq_level = info.commonExt?.qqLevel && calcQQLevel(info.commonExt.qqLevel) || 0
+      ret.age = info.simpleInfo.baseInfo.age ?? 0
+    }
+    return ret
   }
 }
 
