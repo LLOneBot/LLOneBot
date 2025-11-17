eb2985eb48dca32237cff84af2095ea514431b40
linyuchen <lin.yu.chen@hotmail.com>
2025-11-17 10:12:42 +0800

    doc: update history

--- src/version.ts
+++ src/version.ts
@@ -1,7 +1,7 @@
 import fs from 'fs'
 import packageJson from '../package-dist.json'
 
-export const version = '6.6.1'
+export const version = '6.6.2'
 
 export const writeVersion = ()=>{
   const pkgJsonPath = './package-dist.json'
