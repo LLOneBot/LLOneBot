4fe74a73381e8b051a9cf2b2fe5482c60dcc98d0
linyuchen <lin.yu.chen@hotmail.com>
2025-11-23 13:22:03 +0800

    feat: add optional group_id to GetQQAvatar and update URL handling

--- src/onebot11/action/llonebot/user/GetQQAvatar.ts
+++ src/onebot11/action/llonebot/user/GetQQAvatar.ts
@@ -2,7 +2,8 @@ import { BaseAction, Schema } from '../../BaseAction'
 import { ActionName } from '../../types'
 
 interface Payload {
-  user_id: number | string
+  user_id?: number | string
+  group_id?: number | string
 }
 
 interface Response {
@@ -12,10 +13,19 @@ interface Response {
 export class GetQQAvatar extends BaseAction<Payload, Response> {
   actionName = ActionName.GetQQAvatar
   payloadSchema = Schema.object({
-    user_id: Schema.union([Number, String]).required()
+    user_id: Schema.union([Number, String]),
+    group_id: Schema.union([Number, String]),
   })
 
   protected async _handle(payload: Payload) {
-    return { url: `https://thirdqq.qlogo.cn/g?b=qq&nk=${payload.user_id}&s=640` }
+    if (payload.user_id) {
+      return { url: `https://thirdqq.qlogo.cn/g?b=qq&nk=${payload.user_id}&s=640` }
+    }
+    else if (payload.group_id) {
+      return { url: `https://p.qlogo.cn/gh/${payload.group_id}/${payload.group_id}/0` }
+    }
+    else {
+      throw new Error('未指定 user_id 或 group_id')
+    }
   }
 }
