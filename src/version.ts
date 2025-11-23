978306a6b6466ad238a66d4399acbf55ef1f15da
linyuchen <lin.yu.chen@hotmail.com>
2025-11-23 12:57:39 +0800

    chore: bump version to 6.6.4 and update changelog

--- src/version.ts
+++ src/version.ts
@@ -1,7 +1,7 @@
 import fs from 'fs'
 import packageJson from '../package-dist.json'
 
-export const version = '6.6.3'
+export const version = '6.6.4'
 
 export const writeVersion = ()=>{
   const pkgJsonPath = './package-dist.json'
