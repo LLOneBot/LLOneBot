b4e3f5158de9061893ec88354d402a091af1660b
linyuchen <lin.yu.chen@hotmail.com>
2025-12-09 22:40:10 +0800

    chore: bump version

--- src/version.ts
+++ src/version.ts
@@ -1,7 +1,7 @@
 import fs from 'fs'
 import packageJson from '../package-dist.json'
 
-export const version = '7.0.0'
+export const version = '7.0.1'
 
 export const writeVersion = ()=>{
   const pkgJsonPath = './package-dist.json'
